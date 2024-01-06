9d86366d7930ec29a8ca96eb8c3a1c2497aa7a64
Dave <davenyambune@gmail.com>
2024-01-06 18:19:26 +0300

    nav_bar

--- foreverfinds/urls.py
+++ foreverfinds/urls.py
@@ -21,7 +21,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.IndexPage, name='index')
+    path('', views.IndexPage, name='index'),
+    path('living/', views.LivingPage, name='living'),
+    path('kitchen_dining/', views.Kitchen_DiningPage, name='kitchen_dining'),
+    path('bedroom/', views.BedroomPage, name='bedroom'),
+    path('contact/', views.ContactPage, name='contact'),
+    path('homeoffice/', views.HomeOfficePage, name='homeoffice'),
+    path('Outdoor/', views.OutdoorPage, name='Outdoor')
 ]
 
 urlpatterns += staticfiles_urlpatterns()   
